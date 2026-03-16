b52b6f0346802cac52d4d3e177b60cae7e1f6f4b
Moulik Budhiraja <moulikbudhiraja@gmail.com>
2026-03-16 18:21:02 -0400

    init: setup xcode app

---
+++ Desktop Ralph/Desktop Ralph/Desktop_RalphApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desktop_RalphApp.swift
+//  Desktop Ralph
+//
+//  Created by Moulik Budhiraja on 2026-03-16.
+//
+
+import SwiftUI
+
+@main
+struct Desktop_RalphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
